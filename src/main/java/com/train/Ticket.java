4ad096e35a98374a7152f0a37b805db479d12888
choda1999 <choda720@gmail.com>
2019-03-25 00:47:34 +0800

    Kotlin homework 1st-Ticket_rev.1

--- src/main/java/com/train/Ticket.java
+++ src/main/java/com/train/Ticket.java
@@ -4,9 +4,6 @@ package com.train;
 public class Ticket {
     int total_ticket;
     int roundTrip_ticket;
-    private int oneWay_price = 1000;
-    private int roundTrip_price = 2000;
-    private int rountTrip_freerate = 10;
 
     public Ticket(){
         this.total_ticket = total_ticket;
@@ -14,8 +11,11 @@ public class Ticket {
     }
 
     public int price(){
+        int oneWay_price = 1000;
+        int roundTrip_price = 2000;
+        int rountTrip_rate = 10;
         int oneWay_ticket = total_ticket - roundTrip_ticket;
-        int price = oneWay_ticket*oneWay_price + roundTrip_ticket*roundTrip_price*(100-rountTrip_freerate)/100;
+        int price = oneWay_ticket*oneWay_price + roundTrip_ticket*roundTrip_price*(100-rountTrip_rate)/100;
         return price;
     }
 }
--- src/main/kotlin/com/train/TesterKotlin.kt
+++ src/main/kotlin/com/train/TesterKotlin.kt
@@ -5,13 +5,14 @@ import java.util.*
 fun main() {
     val scanner = Scanner(System.`in`)
     print("Please enter number of tickets:")
-    var toatl_ticket = scanner.nextInt()
+    val toatl_ticket = scanner.nextInt()
     print("How many round-trip tickets:")
-    var roundTrip_ticket = scanner.nextInt()
-    println("Total tickets:"+toatl_ticket)
-    println("Round Trip:" + roundTrip_ticket)
-    var ticket = TicketKt(toatl_ticket,roundTrip_ticket)
-    println("Total:" + ticket.price())
+    val roundTrip_ticket = scanner.nextInt()
+    println("Total tickets: $toatl_ticket")
+    println("Round Trip: $roundTrip_ticket")
+    val ticket = TicketKt(toatl_ticket,roundTrip_ticket)
+    val totalPrice = ticket.price()
+    println("Total: $totalPrice")
 }
 
 class TicketKt(var total_ticket:Int,var roundTrip_ticket:Int){
@@ -21,7 +22,7 @@ class TicketKt(var total_ticket:Int,var roundTrip_ticket:Int){
     var oneWay_ticket = total_ticket - roundTrip_ticket
 
     fun price():Int{
-        var price = oneWay_ticket*oneWay_price + roundTrip_ticket*roundTrip_price*roundTrip_rate/100
+        val price = oneWay_ticket*oneWay_price + roundTrip_ticket*roundTrip_price*roundTrip_rate/100
         return price
     }
  }
